f45524dd42e22e30bf5b9adbf41936b4d34f7f76
David Ascanio <daviddeveloper@outlook.com>
2024-07-30 22:39:48 -0500

    config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/Portfolio_David-Ascanio",
+  base : "/Portfolio_David-Ascanio/",
   plugins: [react()],
 })
